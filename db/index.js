935be92901a2edf9088bfc5ef7cdee3d75498cab
JamieHa8794 <JamieHa8794@gmail.com>
2022-09-01 19:51:32 -0400

    inserted data, created connection

--- db/index.js
+++ db/index.js
@@ -20,10 +20,18 @@ const Team = db.define('team', {
     }
 })
 
+Hero.belongsTo(Team);
+Team.hasMany(Hero);
 
 const syncAndSeed = async () =>{
     try{
         await db.sync({ force: true })
+        const [IronMan, Loki, Thor, CaptinAmerica, Hulk, Thanos, Ultron, Wanda ] = await Promise.all(
+            ['IronMan', 'Loki', 'Thor', 'CaptinAmerica', 'Hulk', 'Thanos', 'Ultron', 'Wanda'].map( hero => Hero.create({ name: hero }))
+        )
+        const [Evil, Good, Neutral] = await Promise.all(
+            ['Evil', 'Good', 'Neutral'].map( team => Team.create({ name: team }))
+        )
     }
     catch(err){
         console.log(err);
